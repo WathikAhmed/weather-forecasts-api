1b1abcbd61ba8e7c430bb31fdbda333141d722ec
WathikAhmed <zerox4224@gmail.com>
2021-10-26 10:28:35 +1100

    npm i axios

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express')();   //  Framework that provides a robust set of features for web and mobile applications. Creating a robust API is quick and easy
 const app = express();
+const cheerio = require('cheerio');     //  Cheerio parses markup and provides an API for traversing/manipulating the resulting data structure.
+const axios = require('axios');         //  Promise based HTTP client for the browser and node.js
 const PORT = 2123;
 
 app.use(express.json())    //  express.json acts as middleware to parse json content
