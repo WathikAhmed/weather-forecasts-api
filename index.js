7d8ec0f1c1f77fee5aa6770a3733bc22a0f501fb
WathikAhmed <zerox4224@gmail.com>
2021-10-26 10:36:56 +1100

    npm i nodemon
    nodemon is a tool that helps develop node.js based applications by automatically restarting the node application when file changes in the directory are detected.

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
+const PORT = 8000;
 const express = require('express')();   //  Framework that provides a robust set of features for web and mobile applications. Creating a robust API is quick and easy
-const app = express();
 const cheerio = require('cheerio');     //  Cheerio parses markup and provides an API for traversing/manipulating the resulting data structure.
 const axios = require('axios');         //  Promise based HTTP client for the browser and node.js
-const PORT = 2123;
 
+const app = express();
 app.use(express.json())    //  express.json acts as middleware to parse json content
 
 app.listen(
     PORT,
-    () => console.log(`This is available on http://localhost:${PORT}`)
+    () => console.log(`Server running on PORT ${PORT}`)
 );
 
 app.get('/endpoint1', (req,res) => {
