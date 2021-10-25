aaa01bccaf316c9f31e31e015ab521f9cf6aaf5e
WathikAhmed <zerox4224@gmail.com>
2021-10-26 07:40:01 +1100

    Added GET for endpoint1

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 const app = require('express')();
-const PORT = 1234;
+const PORT = 1212;
 
 app.listen(
     PORT,
     () => console.log(`This is available on http://localhost:${PORT}`)
 );
+
+app.get('/endpoint1', (req,res) => {
+    res.status(200).send({
+        text:'Hi',
+        text2:'You have reached endpoint 1'
+    })
+});
